124fb647f4fb2b08d4fb07ace9d9a4bc6e307866
kaufman79 <kaufman79@gmail.com>
2024-07-11 08:45:05 -0400

    add style to messages, add threading to reduce lag.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from PyQt6.QtWidgets import QMainWindow, QTextEdit, QLineEdit, QPushButton, QApplication
 import sys
 from backend import Chatbot
+import threading
 
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.chatbot = Chatbot()
 
         self.setMinimumSize(700, 500)
 
@@ -17,6 +20,7 @@ class ChatbotWindow(QMainWindow):
         # Add input field widget
         self.input_field = QLineEdit(self)
         self.input_field.setGeometry(10, 340, 480, 40)
+        self.input_field.returnPressed.connect(self.send_message)
 
         # Add button
         self.button = QPushButton("Send", self)
@@ -27,14 +31,16 @@ class ChatbotWindow(QMainWindow):
 
     def send_message(self):
         user_input = self.input_field.text().strip()
-        self.chat_area.append(f"Me: {user_input}")
+        self.chat_area.append(f"<p style='color:#333333'>Me: {user_input}</p>")
         self.input_field.clear()
 
-        chatbot = Chatbot()
-        response = chatbot.get_response(user_input)
-        self.chat_area.append(f"Gippiti: {response}")
+        thread = threading.Thread(target=self.get_bot_response, args=(user_input,))
+        thread.start()
 
-
+    def get_bot_response(self, user_input):
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f"<p style='color:#333333; background-color: #E9E9E9'>"
+                              f"Gippiti: {response}</p>")
 
 
 app = QApplication(sys.argv)
